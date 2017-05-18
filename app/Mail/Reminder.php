3d3efcdc9366825a5ff632bac90f5257345be32b
Xinh Nguyen <ntxinh@tintansoft.com>
2017-05-18 17:19:29 +0700

    edit mail

--- app/Mail/Reminder.php
+++ app/Mail/Reminder.php
@@ -38,24 +38,6 @@ class Reminder extends Mailable
      */
     public function build()
     {
-        $this->tray    = Device::find($this->tray_id);
-        $this->cabinet = Device::find($this->tray->parent_id);
-        $io_center     = IOCenter::find($this->cabinet->io_center_id);
-
-        $this->distributor = Distributor::find($io_center->dis_id);
-        $this->supplier    = Supplier::find($this->distributor->sup_id);
-
-        $tray_ids            = Device::whereActive(true)->where('parent_id', $this->cabinet->id)->pluck('id')->toArray();
-        $this->tray_products = ButtonProduct::where('button_products.active', true)
-            ->whereIn('button_id', $tray_ids)
-            ->leftJoin('devices', 'devices.id', '=', 'button_products.button_id')
-            ->leftJoin('products', 'products.id', '=', 'button_products.product_id')
-            ->leftJoin('units', 'units.id', '=', 'products.unit_id')
-            ->leftJoin('product_prices', 'product_prices.product_id', '=', 'products.id')
-            ->select('button_products.total_quantum', 'devices.name as tray_name', 'products.name as product_name', 'products.barcode as product_barcode', 'units.name as unit_name', DB::raw('CONCAT(FORMAT(product_prices.price_input, 0), \' đ\') as fc_price_input'))
-            ->get();
-        if($this->supplier->email)
-            return $this->to($this->supplier->email)->view('mails.reminder');
         return $this->to('ntxinh@tintansoft.com')->view('mails.reminder');
     }
 }
