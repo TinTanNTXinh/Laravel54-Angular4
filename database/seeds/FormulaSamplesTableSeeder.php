edccbb8baf4684b0addfc6bafbde1d1a41a09f0e
tintanntxinh <ntxinh@tintansoft.com>
2017-05-26 21:13:29 +0700

    seeder formula sample

--- database/seeds/FormulaSamplesTableSeeder.php
+++ database/seeds/FormulaSamplesTableSeeder.php
@@ -13,19 +13,32 @@ class FormulaSamplesTableSeeder extends Seeder
     public function run()
     {
         $rules = [
-
+            'OIL',
+            'PAIR',
+            'RANGE',
+            'SINGLE',
+            'SINGLE',
+            'SINGLE'
         ];
 
         $names = [
-            ''
+            'Giá dầu',
+            'Tuyến đường',
+            'Khoảng cách',
+            'Mã hàng',
+            '1 chiều/2 chiều',
+            'Loại xe'
         ];
 
-        \App\FormulaSample::create([
-            'code'         => $this->generateCode(\App\FormulaSample::class, 'FORMULASAMPLE'),
-            'rule'         => 'PAIR',
-            'name'         => 'T',
-            'index'        => ++$key,
-            'active'       => true,
-        ]);
+        foreach($names as $key => $name)
+        {
+            \App\FormulaSample::create([
+                'code'         => $this->generateCode(\App\FormulaSample::class, 'FORMULASAMPLE'),
+                'rule'         => $rules[$key],
+                'name'         => $name,
+                'index'        => ++$key,
+                'active'       => true
+            ]);
+        }
     }
 }
